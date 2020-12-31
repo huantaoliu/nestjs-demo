9bf6be90772a351a29aff6e77200d860b9bf9ab8
huantaoliu <huantaonick@gmail.com>
2020-12-31 15:31:10 -0500

    feat(fundamental): controller, use query variable

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Get, HttpCode, Post, Redirect } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Redirect,
+} from '@nestjs/common';
+import { UserCRUDDTO } from '../model/user.crud.dto';
 import { UserDTO } from '../model/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-
   constructor(private userServie: UserService) {}
 
   @Get()
@@ -12,16 +22,31 @@ export class UserController {
     return this.userServie.getUsers();
   }
 
+  @Get(':id')
+  findOne(@Param('id') id: string): UserDTO {
+    return this.userServie.getUserById(Number(id));
+  }
+
   @Post()
   @HttpCode(201) // custom return code
-  create(@Body() user: UserDTO) {
+  create(@Body() user: UserCRUDDTO) {
     return this.userServie.createUser(user);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUser: UserCRUDDTO) {
+    return this.userServie.update(Number(id), updateUser);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userServie.deleteUserById(Number(id));
   }
 
   @Get('/help')
   @Redirect('http://docs.nestjs.com/controller', 302)
   userHelp() {
     const section = 'redirection';
-    return {url: 'https://docs.nestjs.com/controller#'+ section}
+    return { url: 'https://docs.nestjs.com/controller#' + section };
   }
 }
