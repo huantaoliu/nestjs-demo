1c3a25514dc43d8b9ea345ceffbd7f0346505d30
huantaoliu <huantaonick@gmail.com>
2021-01-17 16:29:03 -0500

    feat(swagger): add swagger for login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Request,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './guard/local-auth-guard';
+import { LoginRequestDTO } from './model/login.request.dto';
+import { LoginResponseDTO } from './model/login.response.dto';
 
 @Controller()
 export class AppController {
@@ -8,7 +18,21 @@ export class AppController {
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
+  @ApiTags('Auth')
+  @ApiResponse({ status: 401, description: 'unauthorized' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({
+    status: 201,
+    description: 'Created',
+    type: LoginResponseDTO,
+  })
+  async login(
+    @Request() req,
+    @Body(new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true }))
+    body: LoginRequestDTO,
+  ) {
+    // Body is only used for validation, and LocalAuthGuard will check user against db
+    // if pass, guard will attach user obj to request, then we use it to generate JWT token
     return this.authService.login(req.user);
   }
 }
