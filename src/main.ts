fc748aafcb08bbbdf5135655e036bf58f3b3fcf7
huantaoliu <huantaonick@gmail.com>
2021-01-17 14:13:49 -0500

    feat(docker): enable cors config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.enableCors({ origin: true });
+
+  const port = app.get(ConfigService).get('APP_PORT');
+  await app.listen(port || 3000);
 }
 bootstrap();
