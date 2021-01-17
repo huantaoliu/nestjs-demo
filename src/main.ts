5644e0b9783849a633911c502dd686cbde9ca400
huantaoliu <huantaonick@gmail.com>
2021-01-17 14:30:05 -0500

    feat(swagger): setup empty swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
-  app.enableCors({ origin: true });
 
+  // enable cors
+  app.enableCors({ origin: true });
+  // setup swagger
+  const options = new DocumentBuilder()
+    .setTitle('My Nestjs Demo')
+    .setDescription('User article management')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+  // listen on port
   const port = app.get(ConfigService).get('APP_PORT');
   await app.listen(port || 3000);
 }
